190d68de9a06806365741325d135afa90fbafb4b
Alexander F Rødseth <rodseth@gmail.com>
2018-08-22 18:15:11 +0200

    Add test that fails

---
+++ pf_test.go
@@ -0,0 +1,26 @@
+package pf
+
+import (
+	"os"
+	"testing"
+	"fmt"
+)
+
+func TestMap(t *testing.T) {
+	// Define a 10x10 image
+	var pitch int32 = 10
+	pixels := make([]uint32, 10*pitch)
+
+	// Use 1 core for setting all pixels to 42
+	Map(1, func(x uint32) uint32 {
+		return 42
+	}, pixels)
+
+	// Check that all pixels are now 42
+	for i, p := range pixels {
+		if p != 42 {
+			fmt.Fprintf(os.Stderr, "Map fail at pixel %d, y %d. Has value: %d\n", i, int32(i) / pitch, p)
+			t.Fail()
+		}
+	}
+}
